7567d73fba0b9b9c0d252d268fce837b1c6e04d8
DaoDev <boynguyen1998@gmail.com>
2021-05-11 15:05:20 +0700

    fix ui edit text

--- redux-in-reactjs/src/reducers/index.js
+++ redux-in-reactjs/src/reducers/index.js
@@ -1,11 +1,12 @@
 //rootReducer : Tong hop laij tat ca reducer trong app reactJs
-import { combineReducers } from "redux"
-import useReducer from "./user"
-import hobbReducer from "./hobby"
+import { combineReducers } from "redux";
+import useReducer from "./user";
+import hobbyReducer from "./hobby";
 
-const rootReducer = combineReducers({ //ham combineReducers de tong hop tat ca Reducer co trong app reactJs
-    hobby: hobbReducer,
-    user: useReducer
-})
+const rootReducer = combineReducers({
+  //ham combineReducers de tong hop tat ca Reducer co trong app reactJs
+  hobby: hobbyReducer,
+  user: useReducer,
+});
 
-export default rootReducer
+export default rootReducer;
