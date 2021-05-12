42db2a0631a0efcf3dadbc17e8218c62a41fe48a
DaoDev <boynguyen1998@gmail.com>
2021-05-12 16:05:06 +0700

    bind custom control in Formik

--- redux-photo-app/src/features/Photo/components/PhotoForm/index.jsx
+++ redux-photo-app/src/features/Photo/components/PhotoForm/index.jsx
@@ -1,63 +1,69 @@
-import { PHOTO_CATEGORY_OPTIONS } from 'constants/global';
-import Images from 'constants/images';
-import { Formik, Form } from 'formik';
-import PropTypes from 'prop-types';
-import React from 'react';
-import Select from 'react-select';
-import { Button, FormGroup, Input, Label } from 'reactstrap';
+import { PHOTO_CATEGORY_OPTIONS } from "constants/global";
+import Images from "constants/images";
+import InputField from "custom-fields/InputField";
+import RandomPhotoField from "custom-fields/RandomPhotoFeild";
+import SelectField from "custom-fields/SelectField";
+import { Formik, Form, FastField } from "formik";
+import PropTypes from "prop-types";
+import React from "react";
+import { Button, FormGroup, Label } from "reactstrap";
 
 PhotoForm.propTypes = {
-    onSubmit: PropTypes.func,
+  onSubmit: PropTypes.func,
 };
 
 PhotoForm.defaultProps = {
-    onSubmit: null,
-}
+  onSubmit: null,
+};
 
 function PhotoForm(props) {
-    return (
+  const initialValues = {
+    title: "",
+    categoryId: null,
+  };
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values) => console.log("Submit: ", values)}
+    >
+      {(formikProps) => {
+        // do something here ...
+        const { values, errors, touched } = formikProps;
+        console.log({ values, errors, touched });
 
-        <Formik>
-            {formikProps => {
-                const { values, errors, touched } = formikProps
-                console.log({ values, errors, touched });
-                return (
-                    <Form>
-                        <FormGroup>
-                            <Label for="titleId">Title</Label>
-                            <Input name="title" id="titleId" placeholder="Eg: Wow nature ..." />
-                        </FormGroup>
+        return (
+          <Form>
+            <FastField
+              name="title"
+              component={InputField}
+              label="Title"
+              placeholder="Eg: Wow nature ..."
+            />
 
-                        <FormGroup>
-                            <Label for="category">Category</Label>
-                            <Select
-                                id="categoryId"
-                                name="categoryId"
-                                placeholder="What's your photo category??"
-                                options={PHOTO_CATEGORY_OPTIONS}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="category">Photo</Label>
-                            <div>
-                                <Button type="button" outline color="primary">
-                                    Random a photo
-                                </Button>
-                            </div>
-                            <div>
-                                <img width="200px" height="200px" src={Images.COLORFUL_BG} alt="colorfull" />
-                            </div>
-                        </FormGroup>
+            <FastField
+              name="categoryId"
+              component={SelectField}
+              label="Category"
+              placeholder="What's your photo category?"
+              options={PHOTO_CATEGORY_OPTIONS}
+            />
 
-                        <FormGroup>
-                            <Button color="primary">Add to album</Button>
-                        </FormGroup>
-                    </Form>
-                )
+            <FastField
+              name="photo"
+              component={RandomPhotoField}
+              label="Photo"
+            />
 
-            }}
-        </Formik>
-    );
+            <FormGroup>
+              <Button type="submit" color="primary">
+                Add to album
+              </Button>
+            </FormGroup>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
 }
 
 export default PhotoForm;
